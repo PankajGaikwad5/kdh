f8ed5e0b5bc19ee8e4778be73196f7a94d98628b
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-06-24 15:43:38 +0530

    fix: adjust max-width for Navbar component on larger screens

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -127,7 +127,7 @@ const Navbar = ({ isBgBlack, arrow, escape }) => {
             ? 'absolute w-full h-screen bottom-[100%]  flex flex-col p-4 justify-center items-start md:max-w-[13rem] text-gray-800 duration-500 z-20 uppercase'
             : `fixed w-full h-screen left-0 bottom-0 flex flex-col uppercase  ${
                 isBgBlack ? 'text-white' : 'text-white'
-              }  justify-center items-start md:max-w-[13rem] tracking-widest bg-black/90 p-4 z-20 duration-500 `
+              }  justify-center items-start md:max-w-[13rem] 2xl:max-w-[20rem] tracking-widest bg-black/90 p-4 z-20 duration-500 `
         }
       >
         <ul
