d4fccccc6bb2df93e437d4b8dbedf9dc7254606c
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-09-08 15:37:02 +0530

    footer changes

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -31,14 +31,14 @@ const Footer = () => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
 
   return (
-    <div className='flex flex-col bg-black text-sm font-semibold relative z-10 2xl:text-3xl'>
+    <div className='flex flex-col bg-black text-sm font-semibold relative z-10 2xl:text-2xl'>
       <div className='py-2 flex-col w-full flex md:flex-row md:justify-evenly items-center space-x-1 border-t'>
         <div className='flex py-4 justify-center items-center px-4'>
           <Link href='/'>
             <img
               src='/assets/kdhlogo3.png'
               alt=''
-              className='w-48 md:w-40 2xl:w-96'
+              className='w-48 md:w-40 2xl:w-80'
             />
           </Link>
         </div>
@@ -62,7 +62,7 @@ const Footer = () => {
               {link.label}
             </Link>
           ))}
-          <div className='my-4 md:my-0 flex gap-4 items-center py-4 md:py-0 justify-center xl:text-3xl'>
+          <div className='my-4 md:my-0 flex gap-4 items-center py-4 md:py-0 justify-center xl:text-xl'>
             <Link
               href='https://www.instagram.com/karandesaihome/'
               target='_blank'
