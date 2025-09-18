b7a71e03fa6bba83ad11195553ff14143e9fce18
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2025-09-18 13:22:19 +0530

    fixes

--- src/app/productdetails/[id]/page.js
+++ src/app/productdetails/[id]/page.js
@@ -277,7 +277,7 @@ const ProductDetailsPage = () => {
       className={`min-h-screen bg-black text-white font-sans relative ${montserrat.className}`}
     >
       <Navbar />
-      <header className='fixed top-4 right-0 w-full flex justify-end items-center p-3 z-30 bg-black/80 backdrop-blur'>
+      <header className='fixed top-3 right-0 w-full flex justify-end items-center p-4 z-30'>
         {/* <a href='/'>
           <Image
             src='/assets/kdhlogo3.png'
@@ -549,17 +549,17 @@ const ProductDetailsPage = () => {
                 {product.description}
               </div>
 
-              <div className='mt-10 flex gap-4 flex-col md:flex-row '>
+              <div className='mt-10 flex gap-4 flex-col md:flex-row flex-wrap'>
                 <a href={product.pdf} target='_blank' rel='noopener noreferrer'>
                   <Button
                     variant='outline'
-                    className=' px-6 py-2 border w-full md:w-auto border-white text-black rounded-none hover:bg-white/80'
+                    className=' px-6 py-2 border w-full  md:w-auto border-white text-black rounded-none hover:bg-white/80'
                   >
                     Download Spec Sheet
                   </Button>
                 </a>
                 <Button
-                  className=' px-6 py-2 border w-full md:w-auto border-white bg-transparent text-white rounded-none hover:bg-white hover:text-black'
+                  className=' px-6 py-2 border w-full  md:w-auto border-white bg-transparent text-white rounded-none hover:bg-white hover:text-black'
                   onClick={() => setShowModal(true)}
                 >
                   Enquire
