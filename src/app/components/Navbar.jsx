4e6a2727d83cb345a08b23504c9ad0738279377d
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2026-06-04 12:54:52 +0530

    small nav fix

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -189,10 +189,10 @@ const Navbar = ({ isBgBlack, arrow, escape, home }) => {
       <ul
         className={
           !nav
-            ? 'absolute w-full h-screen bottom-[100%] flex flex-col p-4 justify-center items-start md:max-w-[13rem] text-gray-800 duration-500 z-20 uppercase'
+            ? 'absolute w-full h-screen bottom-[100%] flex flex-col p-4 justify-center items-start md:max-w-[13rem] text-gray-800 duration-500 z-50 uppercase'
             : `fixed w-full h-screen left-0 bottom-0 flex flex-col uppercase ${
                 isBgBlack ? 'text-white' : 'text-white'
-              } justify-center items-start md:max-w-[13rem] 2xl:max-w-[20rem] tracking-widest bg-black/90 p-4 z-20 duration-500`
+              } justify-center items-start md:max-w-[13rem] 2xl:max-w-[20rem] tracking-widest bg-black/90 p-4 z-50 duration-500`
         }
       >
         <ul
