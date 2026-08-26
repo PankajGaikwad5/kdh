7a367506e59f8eb41593cbc3b1445001b27fa591
Pankaj Gaikwad <gaikwadpankaj62@gmail.com>
2026-08-26 13:38:42 +0530

    3.0 catalogue

--- src/app/components/catalogues.js
+++ src/app/components/catalogues.js
@@ -14,7 +14,7 @@ export const catalogues = [
   },
   {
     group: 'monster_3.0',
-    pdf: 'https://7h4qznnnsa.ufs.sh/f/8EYZaNz64oKUDEop7R8FYI8sxbtc2ngQN4ZeSPwOKkv5yAjm',
+    pdf: 'https://7h4qznnnsa.ufs.sh/f/8EYZaNz64oKUuyfI12VvD2FOkC3NsiU7yPRWb18aeZThVzGr',
     image: '/assets/dimensions1.png',
     year: '2025',
   },
